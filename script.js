9f65a1119d043749439674b95d68009f5b283e65
bbrryyyn <bryanjamesbondoc@gmail.com>
2026-07-12 01:15:02 +0800

    6

--- script.js
+++ script.js
@@ -9,86 +9,84 @@ const continueBtn = document.getElementById("continueBtn");
 
 const letterText = document.getElementById("letterText");
 
-const fullLetter = `I made this because I wanted to try something creative, as someone na not creative? Hahaha it's explore and try lang. But it turned out to be pretty fun.
+const fullLetter = [
+`I made this because I wanted to try something creative, as someone na not creative? Hahaha it's explore and try lang. But it turned out to be pretty fun.`,
 
-Since it's my birthday, I thought I'd ask if you'd like to celebrate with me if you're up for it.
+`Since it's my birthday, I thought I'd ask if you'd like to celebrate with me if you're up for it.`,
 
-But please don't feel pressured to say yes because of this or because it's my birthday. I know you've been carrying a lot lately. If you're not feeling up to it, that's completely okay. We can celebrate another time whenever you're ready.`;
+`But please don't feel pressured to say yes because of this or because it's my birthday. I know you've been carrying a lot lately. If you're not feeling up to it, that's completely okay. We can celebrate another time whenever you're ready.`
+];
 
 function switchPage(current, next){
 
-    current.style.opacity="0";
+    current.style.opacity = "0";
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
-        current.style.display="none";
+        current.style.display = "none";
 
-        next.style.display="flex";
+        next.style.display = "flex";
+        next.style.opacity = "0";
 
-        next.style.opacity="0";
-
-        requestAnimationFrame(()=>{
-
-            next.style.opacity="1";
-
+        requestAnimationFrame(() => {
+            next.style.opacity = "1";
         });
 
-    },400);
+    }, 400);
 
 }
 
-function typeLetter(){
+async function typeLetter(){
 
-    letterText.innerHTML="";
+    letterText.innerHTML = "";
+    continueBtn.style.display = "none";
 
-    continueBtn.style.display="none";
+    for(const paragraph of fullLetter){
 
-    const words = fullLetter.split(" ");
+        const words = paragraph.split(" ");
 
-    let index = 0;
+        for(const word of words){
 
-    const timer = setInterval(()=>{
+            letterText.innerHTML += word + " ";
 
-        letterText.innerHTML += words[index] + " ";
-
-        index++;
-
-        if(index >= words.length){
-
-            clearInterval(timer);
-
-            continueBtn.style.display="block";
+            await new Promise(resolve => setTimeout(resolve, 150));
 
         }
 
-    },120);
+        letterText.innerHTML += "<br><br>";
+
+        await new Promise(resolve => setTimeout(resolve, 700));
+
+    }
+
+    continueBtn.style.display = "block";
 
 }
 
-openBtn.onclick=function(){
+openBtn.onclick = function(){
 
-    switchPage(landing,envelope);
-
-};
-
-seal.onclick=function(){
-
-    document.querySelector(".flap").style.transform="rotateX(180deg)";
-
-    seal.style.display="none";
-
-    setTimeout(()=>{
-
-        switchPage(envelope,letter);
-
-        setTimeout(typeLetter,500);
-
-    },500);
+    switchPage(landing, envelope);
 
 };
 
-continueBtn.onclick=function(){
+seal.onclick = function(){
 
-    switchPage(letter,invite);
+    document.querySelector(".flap").style.transform = "rotateX(180deg)";
+
+    seal.style.display = "none";
+
+    setTimeout(() => {
+
+        switchPage(envelope, letter);
+
+        setTimeout(typeLetter, 500);
+
+    }, 500);
+
+};
+
+continueBtn.onclick = function(){
+
+    switchPage(letter, invite);
 
 };
